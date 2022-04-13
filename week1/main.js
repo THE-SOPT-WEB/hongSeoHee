c34321402656fa8f5cac51e2c8cce4b81469ae62
seohee0112 <ghdtj7@naver.com>
2022-04-13 11:24:44 +0900

    feat : ✨ 버거 선택시 ul에 li붙고 input, button에 이벤트함수 연결 기능 추가
    이벤트 함수안에 로직 짜기전입니다!

--- week1/main.js
+++ week1/main.js
@@ -1,20 +1,33 @@
 let burgerCardList = document.querySelectorAll(".burger__card");
 let cartPrice = document.querySelector(".cart__price");
 let cartList = document.querySelector(".cart__list");
+function changeBurgerCount(e) {
+  console.log(e);
+}
+function removeBurger(e) {
+  console.log(e.target);
+}
 
 function addCartList(e) {
   const selectBurgerCard = e.currentTarget.querySelector(".burger__content");
   const burgerInfo = {
-    name: selectBurgerCard
-      .querySelector(".burger__name")
-      .innerText.replaceAll(" ", ""),
+    name: selectBurgerCard.querySelector(".burger__name").innerText,
     price: selectBurgerCard.querySelector(".burger__price").innerText,
   };
-  //   console.log(burgerInfo);
-  //   console.log(cartList);
   const li = document.createElement("li");
-  li.classList.add(burgerInfo.name);
-  //   console.log(li);
+  const className = burgerInfo.name.replaceAll(" ", "");
+
+  li.classList.add(className);
+  li.innerHTML = `${burgerInfo.name}
+            <input class=${className} type="number" name="burgerCount" value="1" min="1">
+            ${burgerInfo.price}
+            <button class=${className}>X</button>`;
+  cartList.appendChild(li);
+  const eventNodes = document.querySelectorAll(`.${className}`);
+  // 버거 수량 위아래 조정
+  eventNodes[1].addEventListener("change", changeBurgerCount);
+  // 버거 삭제 버튼
+  eventNodes[2].addEventListener("click", removeBurger);
 }
 
 burgerCardList.forEach((burgerCard) => {
