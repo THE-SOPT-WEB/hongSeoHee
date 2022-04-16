d7ee7afda596894bc279eef18c11570b22060e3a
seohee0112 <ghdtj7@naver.com>
2022-04-16 10:25:36 +0900

    gear : ⚙️  디즈니 테마에 맞춰서 사진 가져오고, 텍스트 영어로 수정

--- week2/main.js
+++ week2/main.js
@@ -1,8 +1,8 @@
-import pic1 from "./assets/김규민.jpeg";
-import pic2 from "./assets/전희선.jpeg";
-import pic3 from "./assets/서혜은.jpg";
-import pic4 from "./assets/황주희.jpeg";
-import pic5 from "./assets/백지연.png";
+import pic1 from "./assets/더키버니.jpeg";
+import pic2 from "./assets/덤퍼.jpeg";
+import pic3 from "./assets/라푼젤.jpeg";
+import pic4 from "./assets/바넬로피.jpeg";
+import pic5 from "./assets/브루니.jpeg";
 
 const $ = (selector) => document.querySelector(selector);
 
@@ -11,24 +11,22 @@ let currentStep = 0;
 const quizList = [
   {
     src: pic1,
-    answer: "김규민",
+    answer: "Ducky & Bunny",
   },
   {
     src: pic2,
-    answer: "전희선",
+    answer: "Thumper",
   },
   {
     src: pic3,
-    answer: "서혜은",
+    answer: "Rapunzel",
   },
   {
     src: pic4,
-    answer: "황주희",
+    answer: "Vanellope",
   },
   {
     src: pic5,
-    answer: "백지연",
+    answer: "Bruni",
   },
 ];
-
-
