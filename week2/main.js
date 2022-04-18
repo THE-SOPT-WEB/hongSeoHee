becd7a29f007ac4c8165ccffb6c5310817108aaa
seohee0112 <ghdtj7@naver.com>
2022-04-18 10:56:06 +0900

    feat : ✨  전체 게임 관리,게임 초기화, 클릭이벤트,모달 함수 및 quizList
    1. 퀴즈 리스트 안에 모달 content영화별로 넣어주기
    2. 전체 게임 매니저 함수로 시작 -> 초기화 -> 클릭이벤트 -> 정답/오답 별 함수 실행
    
    현재 정답 모달 보여주는 함수까지 구현 완료
    오답처리 및 다음 단계 구현 필요

--- week2/main.js
+++ week2/main.js
@@ -1,28 +1,76 @@
 const $ = (selector) => document.querySelector(selector);
-const answerList = $(".answer__list");
-console.log(answerList);
-// console.log("gg");
+
 let currentStep = 0;
 
 const quizList = [
   {
-    src: "./assets/더키버니.jpeg",
+    src: "./assets/더키와버니.gif",
     answer: "Ducky & Bunny",
+    modalContent: "There's so much fluff",
   },
   {
-    src: "./assets/덤퍼.jpeg",
+    src: "./assets/덤퍼정답.gif",
     answer: "Thumper",
+    modalContent: "If you're not going to say nice things, don't say anything",
   },
   {
-    src: "./assets/라푼젤.jpeg",
+    src: "./assets/라푼젤.gif",
     answer: "Rapunzel",
+    modalContent: "Do what you like love what you do",
   },
   {
-    src: "./assets/바넬로피.jpeg",
+    src: "./assets/바넬로피.gif",
     answer: "Vanellope",
+    modalContent: "I like it the way it is",
   },
   {
-    src: "./assets/브루니.jpeg",
+    src: "./assets/브루니.gif",
     answer: "Bruni",
+    modalContent: "❄️ You will always have me ❄️ ",
   },
 ];
+// 게임 초기화
+function initGame({ score, image }) {
+  console.log(score);
+  console.log(image);
+  image.src = quizList[currentStep].src;
+}
+// 정답 했을때 함수
+function showModal(modalContent) {
+  const modal = $(".modal");
+  const modalBody = $(".modal__body");
+  modalBody.innerText = modalContent;
+  modal.classList.remove("hide");
+
+  setTimeout(() => {
+    modal.classList.add("hide");
+  }, 3000);
+}
+// 오류 클릭했을 때 함수
+// 클릭 이벤트 함수
+function clickEvent({ answer }) {
+  answer.addEventListener("click", (e) => {
+    if (e.target instanceof HTMLLIElement) {
+      const clickAnswer = e.target.innerText;
+      const currentAnswer = quizList[currentStep].answer;
+
+      if (clickAnswer === currentAnswer) {
+        showModal(quizList[currentStep].modalContent);
+      } else {
+        console.log("오류");/
+      }
+    }
+  });
+}
+// 전체 게임 관리
+function gameManger(gameInfo) {
+  initGame(gameInfo);
+  clickEvent(gameInfo);
+}
+window.onload = () => {
+  gameManger({
+    score: $(".scoreBoard__score"),
+    answer: $(".answer__list"),
+    image: $(".imageBoard > img"),
+  });
+};
