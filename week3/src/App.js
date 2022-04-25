e3893c2240cec8d646d3807835f82db82de408ef
seohee0112 <ghdtj7@naver.com>
2022-04-25 10:42:58 +0900

    css : 🎨  전역상태 및 theme 생성 및 적용

--- week3/src/App.js
+++ week3/src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
-
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+import { theme } from "./styles/theme";
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
