bb45415feb8313d39685bd6276b91f275cfa4135
seohee0112 <ghdtj7@naver.com>
2022-04-27 15:06:29 +0900

    feat : ✨  이미지 배열로 가져오기 구현

---
+++ week3/src/assets/images/index.js
@@ -0,0 +1,52 @@
+import image1 from "./망상토끼.gif";
+import image2 from "./몰티즈.gif";
+import image3 from "./세숑.gif";
+import image4 from "./슈야.gif";
+import image5 from "./앙꼬.gif";
+import image6 from "./엥.gif";
+import image7 from "./옴팡이.gif";
+import image8 from "./찌미.gif";
+import image9 from "./호두두.gif";
+import image10 from "./푸루와배코.gif";
+export const emoticonItems = [
+  {
+    name: "망상토끼",
+    url: image1,
+  },
+  {
+    name: "몰티즈",
+    url: image2,
+  },
+  {
+    name: "세숑",
+    url: image3,
+  },
+  {
+    name: "슈야",
+    url: image4,
+  },
+  {
+    name: "앙꼬",
+    url: image5,
+  },
+  {
+    name: "엥",
+    url: image6,
+  },
+  {
+    name: "옴팡이",
+    url: image7,
+  },
+  {
+    name: "찌미",
+    url: image8,
+  },
+  {
+    name: "호두두",
+    url: image9,
+  },
+  {
+    name: "푸루와 배코",
+    url: image10,
+  },
+];
