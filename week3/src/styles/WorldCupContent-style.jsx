4a85b17c3fb5dd6da8f1244058064ee69c7b3e26
seohee0112 <ghdtj7@naver.com>
2022-04-27 12:04:17 +0900

    css : 🎨  월드컵 컨텐트 styled 생성

---
+++ week3/src/styles/WorldCupContent-style.jsx
@@ -0,0 +1,41 @@
+import styled, { css } from "styled-components";
+
+const WorldCupDiv = styled.div`
+  ${({ theme }) => {
+    return css`
+      display: flex;
+      justify-content: center;
+      width: 100%;
+      height: 80vh;
+      overflow: hidden;
+      background-color: ${theme.colors.yellow};
+    `;
+  }}
+`;
+const ContentImg = styled.div`
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  background: url(${(props) => props.imgUrl});
+  background-repeat: no-repeat;
+  background-position: 50% center;
+  background-size: cover;
+`;
+const ContentTitle = styled.h2`
+  ${({ theme }) => {
+    return css`
+      position: absolute;
+      top: 80%;
+      left: 50%;
+      transform: translateX(-50%);
+
+      font-family: ${theme.fonts.family};
+      font-size: 100px;
+      font-style: italic;
+      color: ${theme.colors.title};
+      text-shadow: 2px 1px 1px #000;
+    `;
+  }}
+`;
+const styledComponents = { WorldCupDiv, ContentImg, ContentTitle };
+export default styledComponents;
