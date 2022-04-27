2335372b00108cd3b18c44d5f11d788397af6964
seohee0112 <ghdtj7@naver.com>
2022-04-27 15:11:16 +0900

    feat : ✨ 이모티콘 랜덤으로 배치, 대결할 이모티콘, 승자 세배열을 모두 state로 구현
    1. useEffect로 랜덤구현
    2. 화면에 보일 이모티콘을 인덱스 0과 1로 접근해서 나타내주기
    3. map을 사용해 구현할때, key를 넣어줌

--- week3/src/components/WorldCupContent.jsx
+++ week3/src/components/WorldCupContent.jsx
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from "react";
 import styledComponents from "../styles/WorldCupContent-style";
+import { emoticonItems } from "../assets/images";
 const { WorldCupDiv, ContentImg, ContentTitle, VersusDiv } = styledComponents;
 
 const WorldCupContent = () => {
+  const [emoticons, setEmoticons] = useState([]);
+  const [displays, setDisplays] = useState([]);
+  const [winners, setWinners] = useState([]);
+
+  useEffect(() => {
+    emoticonItems.sort(() => Math.random() - 0.5);
+    setEmoticons(emoticonItems);
+    setDisplays([emoticonItems[0], emoticonItems[1]]);
+  }, []);
+
   return (
     <WorldCupDiv>
+      {displays.map((display) => {
+        return (
+          <ContentImg key={display.name} imgUrl={display.url}>
+            <ContentTitle key={display.name}>{display.name}</ContentTitle>
+          </ContentImg>
+        );
+      })}
       <VersusDiv>VS</VersusDiv>
     </WorldCupDiv>
   );
