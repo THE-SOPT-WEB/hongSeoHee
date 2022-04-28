17adef69b4e8835b85dbdccebf5493c2605c709d
seohee0112 <ghdtj7@naver.com>
2022-04-28 12:57:25 +0900

    edit : 🔥  content style 한곳으로 묶어주면서 필요없는 스타일 파일 삭제

--- week3/src/styles/Content-style.jsx
+++ week3/src/styles/Content-style.jsx
@@ -1,38 +1,65 @@
 import styled, { css } from "styled-components";
 
-const WorldCupDiv = styled.div`
+const ContentWrapper = styled.div`
   ${({ theme }) => {
     return css`
       display: flex;
       justify-content: center;
+      align-items: center;
       width: 100%;
       height: 80vh;
       overflow: hidden;
-      background-color: ${theme.colors.yellow};
     `;
   }}
+`;
+
+const ResultWrapper = styled(ContentWrapper)`
+  flex-direction: column;
 `;
 const ContentImg = styled.div`
   position: relative;
   width: 100%;
+  height: 100%;
   overflow: hidden;
   background: url(${(props) => props.imgUrl});
+  background-size: 100% auto;
   background-repeat: no-repeat;
   background-position: 50% center;
-  background-size: cover;
+  background-size: contain;
 `;
-const ContentTitle = styled.h2`
+const FirecrackerImg = styled(ContentImg)`
+  background-position: ${(props) => props.postion};
+`;
+const ContentName = styled.h2`
   ${({ theme }) => {
     return css`
       position: absolute;
       top: 85%;
       left: 50%;
       transform: translateX(-50%);
-
       font-family: ${theme.fonts.family};
       font-size: 100px;
       font-style: italic;
       color: ${theme.colors.title};
+      text-shadow: 2px 1px 1px #000;
+    `;
+  }}
+`;
+const ResultName = styled(ContentName)`
+  top: 70%;
+  font-style: normal;
+`;
+const GameButton = styled.div`
+  ${({ theme }) => {
+    return css`
+      height: fit-content;
+      padding: 20px;
+      margin: 20px;
+      background-color: ${theme.colors.yellow};
+      border-radius: 40px;
+      font-family: ${theme.fonts.family};
+      font-size: 100px;
+      color: ${theme.colors.brown};
       text-shadow: 2px 1px 1px #000;
     `;
   }}
@@ -54,5 +81,14 @@ const VersusDiv = styled.div`
     `;
   }}
 `;
-const styledComponents = { WorldCupDiv, ContentImg, ContentTitle, VersusDiv };
+const styledComponents = {
+  ContentWrapper,
+  ResultWrapper,
+  ContentImg,
+  FirecrackerImg,
+  ContentName,
+  ResultName,
+  GameButton,
+  VersusDiv,
+};
 export default styledComponents;
