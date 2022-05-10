6b18b0692ee7864de6a29725aadb3471010ade20
seohee0112 <ghdtj7@naver.com>
2022-05-10 13:28:13 +0900

    css : 🎨  전역 폰트 적용

--- week4/src/styles/globalStyle.js
+++ week4/src/styles/globalStyle.js
@@ -1,16 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
+@font-face { 
+    font-family: 'LotteMartHappy'; 
+    font-style: normal; 
+    font-weight: 400; 
+    src: url('//cdn.jsdelivr.net/korean-webfonts/1/corps/lottemart/LotteMartHappy/LotteMartHappyMedium.woff') format('woff');
+}
 ${reset}
 #root, body, html {
     width: 100%;
     height: 100%;
-    font-family: 'Dongle', sans-serif;
+    font-family: 'LotteMartHappy';
+
 }
 * {
-    margin: 0,
-    padding: 0;
+    margin: 10%;
+    padding: 10%;
     box-sizing: border-box;
+    }
 `;
 
 export default GlobalStyle;
