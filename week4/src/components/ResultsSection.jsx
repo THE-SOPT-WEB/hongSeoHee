c77c85879046a6ee801c2e6a6d462c1896bcc8ce
seohee0112 <ghdtj7@naver.com>
2022-05-12 12:12:15 +0900

    feat : ✨ 스켈레톤 UI 구현

---
+++ week4/src/components/ResultsSection.jsx
@@ -0,0 +1,20 @@
+import SkeletonStoreArticle from 'components/StoreArticle/SkeletonStoreArticle';
+import StoreArticle from 'components/StoreArticle/StoreArticle';
+import React from 'react';
+function ResultsSection(props) {
+  const { isSearch, results } = props;
+
+  if (isSearch) return new Array(10).fill(1).map((_, i) => <SkeletonStoreArticle key={i} />);
+  if (results.length === 0) return <StoreArticle placeName="❌ 빵집이 없다니ㅠㅠ ❌" />;
+  return results.map(result => (
+    <StoreArticle
+      key={result.id}
+      placeUrl={result.place_url}
+      placeName={`🧁 ${result.place_name}🧁`}
+      roadAddressName={result.road_address_name}
+      phone={result.phone}
+    />
+  ));
+}
+
+export default ResultsSection;
