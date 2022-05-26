4bdfb22cc457c715e253c563de35646e66acc40e
seohee0112 <ghdtj7@naver.com>
2022-05-26 18:04:37 +0900

    feat : ✨ intro, result, worldCup 라우터 구현

--- week3-ts/src/components/common/Router.tsx
+++ week3-ts/src/components/common/Router.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Error404 from '../../pages/Error404';
-import MainGame from '../../pages/MainGame';
+import Intro from '../../pages/Intro';
+import Result from '../../pages/Result';
+import WorldCup from '../../pages/WorldCup';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainGame />} />
+        <Route path="/" element={<Intro />} />
+        <Route path="/worldCup" element={<WorldCup />} />
+        <Route path="/result" element={<Result />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
---
+++ week3-ts/src/pages/Result.tsx
@@ -0,0 +1,12 @@
+import styled, { css } from 'styled-components';
+
+import Header from '../components/common/Header';
+import ResultMain from '../components/Result/ResultMain';
+export default function Result() {
+  return (
+    <>
+      <Header title={'승리자'} />
+      <ResultMain />
+    </>
+  );
+}
