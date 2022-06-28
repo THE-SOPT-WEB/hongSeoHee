ca52097bb863ca4fc4c0c97b8452040dd5ffe2da
seohee0112 <ghdtj7@naver.com>
2022-06-28 23:19:40 +0900

    [ styles ] css 초기 세팅

---
+++ week7-ts/src/styles/styles.d.ts
@@ -0,0 +1,23 @@
+import 'styled-components';
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      letter_purple01: string;
+      letter_purple02: string;
+      letter_purple03: string;
+      letter_purple04: string;
+      letter_white: string;
+      letter_red: string;
+      letter_gray: string;
+    };
+    fonts: {
+      letter_h1: string;
+      letter_h2: string;
+      letter_h3: string;
+      letter_h4: string;
+      letter_b1: string;
+      letter_b2: string;
+      letter_b3: string;
+    };
+  }
+}
