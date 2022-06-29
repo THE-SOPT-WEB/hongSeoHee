ee1e076cae7e615a9f7e1959771025d0dc0f571a
seohee0112 <ghdtj7@naver.com>
2022-06-29 11:31:04 +0900

    [ styles ] theme 적용

---
+++ week8/src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+  yellow: '#FFE266',
+  darkYellow: '#daa520',
+  brown: '#5E2F1A',
+};
+
+const theme: DefaultTheme = {
+  colors,
+};
+export default theme;
